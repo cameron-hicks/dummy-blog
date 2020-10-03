d3cb78c1d5e2308cba62013de3189f2d58eb786e
Cameron Baumgartner <cameron@Camerons-MacBook-Air.local>
2020-10-03 12:56:26 -0400

    outlining todos

---
+++ script.js
@@ -0,0 +1,3 @@
+//Add functionality to like button: if someone hits Like, a like counter increments
+
+//Add functionality to comments input: if someone enters a comment, it appears as a comment div
